c64f8493194ec3ef973496cf01942ce404a8bc42
muhamash <muhammad-ashraful@outlook.com>
2024-07-11 04:04:26 +0600

    like comment implemented

--- src/components/post/PostAction.jsx
+++ src/components/post/PostAction.jsx
@@ -1,19 +1,48 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-// import React from 'react'
-import Comment from '../../assets/icons/comment.svg';
+import React from 'react';
 import Like from '../../assets/icons/like.svg';
+import setLikeI from '../../assets/icons/notification.svg';
 import Share from '../../assets/icons/share.svg';
+import useAuth from '../../hooks/useAuth';
+import useAxios from '../../hooks/useAxios';
 
-export default function PostAction({post}) {
+export default function PostAction ( { post } )
+{
+    const { api } = useAxios();
+    const { auth } = useAuth();
+    const [ like, setLike ] = React.useState(post?.likes?.includes(auth?.user?.id));
+    
+
+    const handleLike = async() =>
+    {
+        try
+        {
+            const response = await api.patch( `http://localhost:3000/posts/${post.id}/like` );
+
+            if ( response.status === 200 )
+            {
+                console.log(response.data)
+                setLike( !like );
+            }
+
+        }
+        catch ( error )
+        {
+            console.log( error );
+            setLike( false );
+        }
+    }
+
     return (
         <div className="flex items-center justify-between py-6 lg:px-10 lg:py-8">
             {/* <!-- Like Button --> */}
             <button
+                onClick={handleLike}
                 className="flex-center gap-2 text-xs font-bold text-[#B8BBBF] hover:text-white lg:text-sm"
             >
-                <img src={Like} alt="Like" />
-                <span>Like</span>
+                <img src={like ? setLikeI : Like} alt="Like" />
+                <span>{ like && "Liked" }</span>
             </button>
 
             {/* <!-- Comment Button --> */}
--- src/components/post/PostComment.jsx
+++ src/components/post/PostComment.jsx
@@ -1,27 +1,58 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import useAuth from '../../hooks/useAuth.jsx';
 import { useAvatar } from "../../hooks/useAvatar";
+import useAxios from '../../hooks/useAxios';
 import CommentList from "./CommentList";
 
 export default function PostComment ( { post } )
 {
+    const { api } = useAxios();
+    const {auth} = useAuth()
     const avatar = useAvatar( post );
     // console.log( avatar, post );
-    const [ comment, setComment ] = React.useState( false );
+    const [ comments, setComments ] = React.useState(post?.comments);
+    const [ comment, setComment ] = React.useState( "" );
+    const [ show, setShow ] = React.useState( false );
 
+    const addComment = async ( event ) =>
+    {
+        try
+        {
+            const keyCode = event.keyCode;
+        
+            if ( keyCode === 13 )
+            {
+                const response = await api.patch( `http://localhost:3000/posts/${post.id}/comment`, { comment } );
+                console.log( response );
+
+                if ( response.status === 200 )
+                {
+                    setComments( [ ...response.data.comments ] );
+                }
+            }
+        }
+        catch ( error )
+        {
+            console.error( error);
+        }
+    };
     return (
         <div>
             {/* <!-- comment input box --> */ }
             <div className="flex-center mb-3 gap-2 lg:gap-4">
                 <img
                     className="max-w-7 max-h-7 rounded-full lg:max-h-[34px] lg:max-w-[34px]"
-                    src={ avatar }
+                    src={ `http://localhost:3000/${auth?.user?.avatar}` }
                     alt="avatar"
                 />
 
                 <div className="flex-1">
                     <input
+                        value={comment}
                         type="text"
+                        onChange={ ( e ) => setComment( e.target.value ) }
+                        onKeyDown={(e) => addComment(e)}
                         className="h-8 w-full rounded-full bg-lighterDark px-4 text-xs focus:outline-none sm:h-[38px]"
                         name="post"
                         id="post"
@@ -31,13 +62,13 @@ export default function PostComment ( { post } )
             </div>
             {/* <!-- comment filter button --> */ }
             <div className="mt-4 flex">
-                <button onClick={ () => setComment( !comment ) } className="text-gray-300 max-md:text-sm ">
+                <button onClick={ () => setShow( !show ) } className="text-gray-300 max-md:text-sm ">
                     All Comment ▾
                 </button>
             </div>
             {
-                comment && (
-                    <CommentList comments={ post?.comments } />
+                show && (
+                    <CommentList comments={ comments } />
                 )
             }
         </div>
